ea504c9c068ddd8fe9ec7b940e6f17081f4fd0c7
annyLu1z4 <anny.costa7@aluno.educa.go.gov.br>
2025-10-28 15:54:30 -0300

    Update login function password validation

--- Login.js
+++ Login.js
@@ -2,7 +2,7 @@ function login() {
     const username = document.getElementById('Email').value;
     const password = document.getElementById('password').value;
 
-    if (username ==='chiclete@gmail.com' && password === '30112007') {
+    if (username ==='chiclete@gmail.com' && password === '03092728') {
         window.location.href = 'index.html';
     } else {
         alert('Informações Incorretas');
@@ -13,4 +13,5 @@ document.addEventListener('keypress', function(event) {
 if (event.key === 'Enter') {
     login();
 }
+
 });
